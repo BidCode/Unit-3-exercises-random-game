c50067aa07ebd938d45e98f61b9054c0852231df
Pierre Bidkhanian <79528930+BidCode@users.noreply.github.com>
2021-02-23 21:52:44 -0800

    Add files via upload

---
+++ githubCode/script.js
@@ -0,0 +1,56 @@
+// // unit 3 exercises
+
+// // -----------------------------------------------------------------
+// // -----------------------------------------------------------------
+
+
+// // Timers Exercise 
+
+// function countDown (num1) {
+//     setInterval(function () {
+//         if (num1 <= 0) {
+//             console.log('DONE!')
+//             clearInterval(1);
+//         } else {
+//             console.log(num1--)
+//         }
+//     },1000)
+  
+// }
+
+// countDown(3);
+
+// // -----------------------------------------------------------------
+// // -----------------------------------------------------------------
+
+
+// // Random Game Exercise 
+
+// function randomGame () {
+   
+//     let counter = 0;
+//     let randNum;
+
+   
+//   let timer =  setInterval(function () {
+//         randNum = Math.random();
+//        console.log(randNum);
+//        counter++;
+//        if(randNum > .8) {
+//            clearInterval(1)
+//            console.log('our game is done');
+//            console.log('counter is ',counter);
+         
+//        }
+       
+//     }, 100);
+
+  
+// }
+
+// randomGame();
+
+
+// // -----------------------------------------------------------------
+// // -----------------------------------------------------------------
+
