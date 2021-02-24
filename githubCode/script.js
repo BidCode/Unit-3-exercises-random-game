ea8724aed9228cd4ef7744b043b096aea645c668
Pierre Bidkhanian <79528930+BidCode@users.noreply.github.com>
2021-02-23 22:01:08 -0800

    Add files via upload

--- githubCode/script.js
+++ githubCode/script.js
@@ -54,3 +54,32 @@
 // // -----------------------------------------------------------------
 // // -----------------------------------------------------------------
 
+// // Random Game Exercise WITH ONE FLAW
+// but the counter wont go up because I didnt assign a variable
+// to my set interval function 
+// why does assigning a variable to this annoymous function 
+// make the counter go up (as in update the variable counter?)
+
+// function randomGame () {
+   
+//     let counter = 0;
+//     let randNum;
+
+   
+//     setInterval(function () {
+//         randNum = Math.random();
+//        console.log(randNum);
+//        counter++;
+//        if(randNum > .8) {
+//            clearInterval(1)
+//            console.log('our game is done');
+//            console.log('counter is ',counter);
+         
+//        }
+       
+//     }, 100);
+
+  
+// }
+
+// randomGame();
